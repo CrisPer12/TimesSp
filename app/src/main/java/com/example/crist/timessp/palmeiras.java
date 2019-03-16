9c01a306b8619a59e44e938e709b68ca0faaf66f
CriStegen12 <cristianopereiraramos9@gmail.com>
2019-03-16 19:56:17 -0300

    fix layout add icones

---
+++ app/src/main/java/com/example/crist/timessp/palmeiras.java
@@ -0,0 +1,13 @@
+package com.example.crist.timessp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class palmeiras extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palmeiras);
+    }
+}
