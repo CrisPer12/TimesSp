ba262977cb29658aa5a2a6481d9a2a97da1f7ec9
CriStegen12 <cristianopereiraramos9@gmail.com>
2019-03-16 22:48:39 -0300

    fu

--- app/src/main/java/com/example/crist/timessp/corin.java
+++ app/src/main/java/com/example/crist/timessp/corin.java
@@ -1,13 +1,26 @@
 package com.example.crist.timessp;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class corin extends Activity {
+
+    private Button btnHino;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_corin);
+
+        btnHino = (Button) findViewById(R.id.hinoCor);
+
+        clickHino();
+
     }
+
 }
